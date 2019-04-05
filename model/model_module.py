2ba5eb5a3ebb413eca75f948a4e949eb2d60efcf
ktkth5 <zcdjkjfygxnc9w88mmt7@gmail.com>
2019-04-05 15:07:20 +0900

    cp

--- model/model_module.py
+++ model/model_module.py
@@ -63,6 +63,7 @@ class FLGC(nn.Module):
             out = None
             debug_num_filter = 0
             print("s t", s.shape, t.shape, sum(t == 0).item(), sum(t == 1).item())
+            print("t", t[:200])
             for i in range(self.group_num):
                 num_input = sum(s==i).item()
                 num_filter = sum(t == i).item()
