17e418704bb9046d379fc334c640402a86f7aa3e
ktkth5 <zcdjkjfygxnc9w88mmt7@gmail.com>
2019-04-06 16:24:53 +0900

    cp

--- model/model_module.py
+++ model/model_module.py
@@ -74,7 +74,7 @@ class FLGC(nn.Module):
                     out = F.conv2d(xi,ti,stride=self.stride,padding=self.padding,dilation=self.dilation)
                 else:
                     out = torch.cat([out, F.conv2d(xi,ti,stride=self.stride,padding=self.padding,dilation=self.dilation)], 1)
-                print("out", out.shape, t_hat, s_hat)
+                print("out",out.shape)
             out = out[:, out_index]
 
 
