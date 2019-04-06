ead0a176f1ef0a8c1286eeeb1a290375f449e772
ktkth5 <zcdjkjfygxnc9w88mmt7@gmail.com>
2019-04-06 20:50:52 +0900

    cp

--- model/model_module.py
+++ model/model_module.py
@@ -45,8 +45,9 @@ class FLGC(nn.Module):
         :return: shape(B, output_channel, H, W)
         """
         # print("self.final_inference", self.final_inference)
+        B, _, H, W = x.size()
         if not self.final_inference: # if self.training
-            B, _, H, W = x.size()
+
             s_hat = torch.softmax(self.S, dim=1)
             t_hat = torch.softmax(self.T, dim=1)
             s = torch.argmax(s_hat, dim=1)
@@ -64,8 +65,6 @@ class FLGC(nn.Module):
                 #     out += F.conv2d(xi,ti,stride=self.stride,padding=self.padding,dilation=self.dilation)
 
                 # New Method
-                # x_index = list(np.where(s.cpu() == i)[0])
-                # f_index = list(np.where(t.cpu() == i)[0])
                 x_index = (s==i).nonzero().view(-1)
                 f_index = (t==i).nonzero().view(-1)
                 # print("x_index f_index", x_index.shape, f_index.shape)
@@ -78,54 +77,40 @@ class FLGC(nn.Module):
                     out = F.conv2d(xi,ti,stride=self.stride,padding=self.padding,dilation=self.dilation)
                 else:
                     out = torch.cat([out, F.conv2d(xi,ti,stride=self.stride,padding=self.padding,dilation=self.dilation)], 1)
-                # print("out",out.shape)
+                # print(x_index,f_index)
+                # print("ti", ti[0,:].view(-1))
+                # print("out",out[:,:,0])
+            range_index = [i for i in range(out.shape[1])]
+            out_index = [list(out_index.cpu()).index(i) for i in range_index]
             out = out[:, out_index]
+            # print("out_index\n", out_index)
+            # print("f out", out[:,:,0])
 
 
             return out
 
         else: # if not self.training
             end = time.time()
-
-            feature_index = [i for i in range(self.input_channel)]
             s_hat = torch.softmax(self.S, dim=1)
-            t_hat = torch.softmax(self.T, dim=1)
-            s = torch.argmax(s_hat, dim=1)
-            t = torch.argmax(t_hat, dim=1)
-            # s = torch.LongTensor([0, 1, 2, 2])
-            # t = torch.LongTensor([0, 1, 1, 2, 2])
             out = None
-            debug_num_filter = 0
             self.time_cp0 += time.time() - end
             end = time.time()
             for i in range(self.group_num):
-                num_input = self.num_input_list[i] # num_input  = torch.sum(s == i).item()
-                num_filter = self.num_filter_list[i] # num_filter = torch.sum(t == i).item()
-                debug_num_filter += num_filter
-                if num_input*num_filter==0:
-                    continue
-                # print(i,"f num input, num filter", num_input, num_filter)
-                group_index = self.group_index_list[i] # group_index = np.where(s.cpu()==i)[0]
-                index_new = self.index_new_list[i] # index_new = [feature_index.index(index) for index in group_index]
-                # print(i,"f input", x[:,index_new,:,:].shape)
-                # print(f"{i} x", x.shape)
+                x_index = self.index_new_list[i]
                 self.time_cp1 += time.time() - end
                 end = time.time()
+                xi = x[:, x_index] * s_hat[x_index, i].view(1, -1, 1, 1).expand(B, len(x_index), H, W)
                 if out is None:
-                    out = self.conv_test[i](x[:,index_new,:,:])
+                    out = self.conv_test[i](xi)
                 else:
-                    out = torch.cat([out, self.conv_test[i](x[:,index_new,:,:])], 1)
+                    out = torch.cat([out, self.conv_test[i](xi)], 1)
+                # print("out", out[:,:,0])
                 self.time_cp2 += time.time() - end
                 end = time.time()
 
             out_new = out[:, self.output_index]
-            # out_new = torch.zeros_like(out)
-            # for i, index in enumerate(self.output_index):
-            #     print("i index", i, index)
-                # out_new[:,index,:,:] = out[:,i,:,:]
             self.time_cp3 += time.time() - end
             # print("TIME", self.time_cp0, self.time_cp1, self.time_cp2, self.time_cp3)
-            # print("S T", s_hat, t_hat)
             return out_new
 
 
@@ -146,28 +131,62 @@ class FLGC(nn.Module):
         self.group_index_list = []
         self.index_new_list = []
         feature_index = [i for i in range(self.input_channel)]
+        filter_index = [i for i in range(self.output_channel)]
         for i in range(self.group_num):
-            num_input = torch.sum(s==i).item()
-            num_filter = torch.sum(t==i).item()
+            x_index = (s == i).nonzero().view(-1)
+            f_index = (t == i).nonzero().view(-1)
+            num_input = len(x_index)
+            num_filter = len(f_index)
             self.num_input_list.append(num_input)
             self.num_filter_list.append(num_filter)
-            self.group_index_list.append(np.where(s.cpu()==i)[0])
-            self.index_new_list.append([feature_index.index(index) for index in self.group_index_list[-1]])
+            self.group_index_list.append(x_index)
+            # self.index_new_list.append([feature_index.index(index) for index in self.group_index_list[-1]])
+            self.index_new_list.append(x_index)
             # print(i,"num input, num filter", num_input, num_filter)
             if num_input*num_filter==0:
                 self.conv_test.append(None)
             else:
                 conv = nn.Conv2d(num_input,num_filter,kernel_size=self.kernel_size,
                                                 stride=self.stride,padding=self.padding,bias=False).to(self.device)
-                conv.weight.data = self.conv[list(np.where(t.cpu()==i)[0])][:,list(np.where(s.cpu()==i)[0])]
+                conv.weight.data = self.conv[f_index][:,x_index] * t_hat[f_index,i].view(-1,1,1,1).expand(len(f_index),len(x_index),1,1)
                 self.conv_test.append(conv)
+                # print("conv.weight",conv.weight.data[0,:].view(-1))
             self.output_index += list(np.where(t.cpu()==i)[0])
+            # print(self.group_index_list[-1], self.output_index[-1])
+        self.output_index = [self.output_index.index(index) for index in filter_index]
         return self.output_index
 
 
-# def cycle_check(seq):
-#     for module in seq.children():
-#         if isinstance()
+def test_training():
+    _input = torch.randn(1, 4, 5, 5)
+    _input[:, [0, 2, 3]] = 0
+    conv = FLGC(4, 5, 1, stride=1, padding=0, dilation=1, bias=False, group_num=2)
+    target = torch.rand(1,5,5,5)
+    criterion = nn.MSELoss()
+    conv.train()
+    optimizer = torch.optim.SGD(conv.parameters(), lr=0.1)
+    conv.eval()
+    for i in range(10):
+        input = _input.clone()
+        out = conv(input)
+        loss = criterion(out, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(loss.item(), conv.conv.mean().item(), conv.S.mean().item(), conv.T.mean().item())
+
+    _input[:, [0,2,3]] = 0
+    out = conv(_input)
+    # print("mid", out[:,:,0])
+    conv.before_inference()
+    conv.eval()
+    with torch.no_grad():
+        input = _input.clone()
+        out = conv(input)
+        loss = criterion(out, target)
+        # print(loss.item())
+    # print("input\n", input[:,:,0])
+    # print("output\n", out[:,:,0])
 
 def apply_module(self):
     if isinstance(self, FLGC):
@@ -193,3 +212,17 @@ def add_eval_set(net_main_module):
     net_main_module.eval_set = eval_set.__get__(net_main_module)
 
     return net_main_module
+
+
+
+if __name__=="__main__":
+    # x = torch.randn(3,2,1,1)
+    # x = torch.softmax(x, dim=1)
+    # t = torch.argmax(x, dim=1).view(-1)
+
+    # print("t", t, t.shape)
+    # print(sum(t==1).item())
+    # print(list(np.where(t==1)[0]))
+
+
+    test_training()
